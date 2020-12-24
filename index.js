54c7c5501685969518c27109eafdec5e29b96d24
sunbot977 <cjsdntjr5@gmail.com>
2020-12-24 20:52:15 +0900

    index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { prefix } = require('./config/bot_info.json')
 const token = process.env.token
 
 client.on('ready', () => {
+	client.user.setPresence({ activity: { name: "!명령어를 입력해보세요." }, status: "online"})
 	fs.readFile('./Discord.js/LICENSE', function(err, data) {
 		if(err) {
 			console.log("!! 라이센스 파일이 존재하지 않습니다. 클라이언트를 종료합니다. !!")
@@ -18,8 +19,7 @@ client.on('ready', () => {
 			console.log("!! 라이센스 파일이 존재하지 않습니다. 클라이언트를 종료합니다. !!") 
 			process.exit()
 		} else {
-			console.log("\nBOT IS READY\n")
-			client.user.setPresence({ activity: { name: "!명령어를 입력해보세요." }, status: "online"})
+			console.log("\n켰다\n")
 		}
 	})
 });
